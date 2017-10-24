5ff94cf23a7f1f618212bcb87fa300caefc589f3
tschreindl <tim.schreindl@cn-consult.eu>
2017-10-24 12:58:17 +0200

    Customized main.js file
    Customized main.js file to display badges.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,25 @@
-$(function() {
+$(function () {
+    $.ajax({
+        url: 'https://www.codeschool.com/users/tschreindl.json',
+        dataType: 'jsonp',
+        success: function (response) {
+            displayBadges(response.courses.completed)
+        }
+    });
 
-  // your code will go here
+    function displayBadges(courses)
+    {
+        courses.forEach(function (course)
+        {
+            var div = $("<div></div>").addClass("course");
+            div.append("<h3>" + course.title + "</h3>");
+            div.append("<img src='" + course.badge + "'/>");
+            div.append("<a href='" + course.url + "' target='_blank' class='btn btn-primary'>See course</a>");
+
+            $("#badges").append(div);
+
+        })
+    }
+
 
 });
